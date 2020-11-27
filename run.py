b0ccea5ce7c59f76d6ca9bd4f77d942b80deffd8
Elif Alkaç <daldalelif88@gmail.com>
2020-11-28 00:13:29 +0100

    edit run experiments

--- run.py
+++ run.py
@@ -16,56 +16,40 @@ import pickle
 import time
 import pandas as pd
 import itertools
+from absl import app, flags
 
-#param grids for models
-param_grid_logit = {
-    'model__C': [1, 10, 100],  #[0.001,0.01,0.1,1,10,100],
-    #'model__penalty': ['l2'],  #['l1', 'l2']
-    #'model__class_weight':[None, 'balanced', ],  
-    #'model__max_iter':[100, 600, ], 
-}
+FLAGS = flags.FLAGS
 
-param_grid_svm = {
-    'model__C': [1, 10, 100],  #[0.001,0.01,0.1,1,10,100],
-}
+# Required parameters
 
-param_grid_cnn = {
-    'model__num_filters': [8], #[120, 100, 80],  #conv output
-    #'model__hidden_dims': [64], #[64, 50, 10], 
-    'model__num_epochs': [30], #[15, 30, 50],
-    'model__filter_sizes':[(2,3,4,5,6), ],
-    #'model__filter_sizes':[(2,3,4,5,6), (2,3,4), (3,4,5)], 
-    #'model__l2':[0.00001, 0.01, 0.1],
-    'model__embedding_dim':[50 ,]
-}
+flags.DEFINE_string(
+    "data_file", None, 
+    "The .csv file that contains data.")
 
+# Other parameters
+flags.DEFINE_integer(
+    "iteration_count", 5, 
+    "")
 
-#param grids for features
-param_grid_sentiment = {
-    'features__sentiment__feature_extraction__score_names': [['neg', 'compound'], ['neg', 'compound', 'neu', 'pos'], ],
-}   
+flags.DEFINE_list(
+    "features", 'sentiment,ngram', 
+    "")
 
-param_grid_ngram = {
-    'features__ngram__feature_extraction__ngram_range': [(1, 2),],
-    #'features__ngram__feature_selection__model': [model, ],
-}   
+flags.DEFINE_list(
+    "models", 'logistic_regression,svm', 
+    "")
 
-param_grid_type_dependency= {
-    'features__type_dependency__feature_extraction__ngram_range': [(1, 1),],
-    'features__type_dependency__feature_extraction__model_path': ['/Users/ezgidaldal/sexism_custom_classifier/stanford-corenlp-4.1.0', ],
-    #'features__type_dependency__feature_selection__model': [model, ],
-}   
+flags.DEFINE_string(
+    "stanford_corenlp_dir", None,
+    "The type dependency parser model directory. It should contain the .jar files (or other data files) for the task.")
 
-param_grid_bert= {
-    'features__bert__feature_extraction__embedding_file_name': ['../src/bert_embeddings/doc_embeddings20201121-161343.pkl', ],
-    'features__bert__feature_extraction__extract': [False, ],
-    'features__bert__feature_extraction__model_name': ['bert-base-uncased', ],
-}   
+flags.DEFINE_string(
+    "bert_doc_emb_file", None, 
+    "")
 
-param_grid_text_vec = {
-    'features__textvec__feature_extraction__max_tokens': [2400, ],
-    'features__textvec__feature_extraction__output_sequence_length': [60, ],
-} 
+flags.DEFINE_string(
+    "bert_word_emb_file", None, 
+    "")
 
 class RunPipeline():
     '''Runs pipeline for a given data domain, model and features.'''
@@ -81,7 +65,7 @@ class RunPipeline():
         return pd.Series(filtered_dict)
         
     @execution_time_calculator
-    def run(self, train_domain, test_domain, models, features_list, iteration_count=1, file_name='results_'):
+    def run(self):
         '''Runs the steps below.
         Step 1. Read data
         Step 2. Split data
@@ -89,16 +73,21 @@ class RunPipeline():
         Step 4. Fit and predict with sklearn GridSearchCV
         Step 5. Get classification report
         '''
+        features_list=self.get_features()
+        models=self.get_models()
+        
+        train_domain=Domain.BHO
+        test_domain=Domain.BHO
         
         # Step 1. Read data
         make_dataset = MakeDataset()
-        data = make_dataset.read_data('../data/raw/all_data_augmented.csv')
+        data = make_dataset.read_data(FLAGS.data_file)
         
         results_df = pd.DataFrame(columns=['train_domain', 'train_domain_modified','test_domain', 'test_domain_modified',
                                             'model_name', 'feature_names', 'param_grid', 'best_params', 
                                             'y_test', 'y_pred', 'y_test_ids'])
         
-        for i in range(iteration_count):
+        for i in range(FLAGS.iteration_count):
         
             # Step 2. Split data
             X_train, y_train, X_test, y_test=make_dataset.get_balanced_data_split(data, train_domain, test_domain)
@@ -134,7 +123,7 @@ class RunPipeline():
         results_df=pd.concat((results_df, metrics_df), axis=1)
         
         timestr = time.strftime("%Y%m%d-%H%M%S")
-        file_name=''.join(('../results/', file_name, timestr, '.pkl'))
+        file_name=''.join(('results/results_', timestr, '.pkl'))
         
         with open(file_name, 'wb+') as f:
             pickle.dump(results_df, f)
@@ -160,12 +149,33 @@ class RunPipeline():
     
         return feature_combinations
     
-    def run_experiments_research_question_1(self, iteration_count=1):
-        train_domain=Domain.BHO
-        test_domain=Domain.BHO
-        
-        models={Model.CNN:param_grid_cnn, }
-        #models={Model.LR:param_grid_logit, Model.SVM:param_grid_svm, }
+    def get_features(self):
+        #param grids for features
+        param_grid_sentiment={
+            'features__sentiment__feature_extraction__score_names': [['neg', 'compound'], ['neg', 'compound', 'neu', 'pos'], ],
+        }   
+
+        param_grid_ngram={
+            'features__ngram__feature_extraction__ngram_range': [(1, 2),],
+            #'features__ngram__feature_selection__model': [model, ],
+        }   
+
+        param_grid_type_dependency={
+            'features__type_dependency__feature_extraction__ngram_range': [(1, 1),],
+            'features__type_dependency__feature_extraction__model_path': [FLAGS.stanford_corenlp_dir, ],
+            #'features__type_dependency__feature_selection__model': [model, ],
+        }   
+
+        param_grid_bert={
+            'features__bert__feature_extraction__embedding_file_name': [FLAGS.bert_doc_emb_file, ],
+            'features__bert__feature_extraction__extract': [False, ],
+            'features__bert__feature_extraction__model_name': ['bert-base-uncased', ],
+        }   
+
+        param_grid_text_vec={
+            'features__textvec__feature_extraction__max_tokens': [2400, ],
+            'features__textvec__feature_extraction__output_sequence_length': [60, ],
+        }
         
         sentiment={'name': Feature.SENTIMENT, 'param_grid': param_grid_sentiment}
         ngram={'name': Feature.NGRAM, 'param_grid': param_grid_ngram}
@@ -173,11 +183,53 @@ class RunPipeline():
         bert={'name': Feature.BERT, 'param_grid': param_grid_bert}
         text_vec={'name': Feature.TEXTVEC, 'param_grid': param_grid_text_vec}
         
-        features=[text_vec, ]
-        #features=[sentiment, ngram, type_dependency, bert]
+        #Define implemented features and parameter grids
+        valid_features=[sentiment, ngram, type_dependency, bert, text_vec]
         
-        features_list=self.get_feature_combinations(features)
-                
-        file_name=self.run(train_domain, test_domain, models, features_list, iteration_count=iteration_count, file_name='results_rq1_')
+        #Filter feature list with FLAGS.features
+        features = [f for f in valid_features if f['name'] in FLAGS.features]
         
-        return file_name
+        #Return feature combinations
+        return self.get_feature_combinations(features)
+        
+    def get_models(self):
+        #param grids for models
+        param_grid_logit={
+            'model__C': [10,], #[1, 10, 100], #[0.001,0.01,0.1,1,10,100],
+            #'model__penalty': ['l2'],  #['l1', 'l2']
+            #'model__class_weight':[None, 'balanced', ],  
+            'model__max_iter':[2500] # [100, 600, ], 
+        }
+
+        param_grid_svm={
+            'model__C': [1, 10, 100],  #[0.001,0.01,0.1,1,10,100],
+        }
+
+        param_grid_cnn={
+            'model__num_filters': [8], #[120, 100, 80],  #conv output
+            #'model__hidden_dims': [64], #[64, 50, 10], 
+            'model__num_epochs': [30], #[15, 30, 50],
+            'model__filter_sizes':[(2,3,4,5,6), ],
+            #'model__filter_sizes':[(2,3,4,5,6), (2,3,4), (3,4,5)], 
+            #'model__l2':[0.00001, 0.01, 0.1],
+            'model__embedding_dim':[50 ,]
+        }
+        
+        valid_models={Model.LR:param_grid_logit, Model.SVM:param_grid_svm, Model.CNN:param_grid_cnn, }
+        return {k:v for (k,v) in valid_models.items() if k in FLAGS.models}
+        
+
+def main(argv):
+    if 'type_dependency' in FLAGS.features and FLAGS.stanford_corenlp_dir is None:
+        raise ValueError("To use type dependency feature, stanford_corenlp_dir should be given.")
+    if 'bert' in FLAGS.features and (FLAGS.bert_doc_emb_file is None or FLAGS.bert_word_emb_file is None):
+        raise ValueError("To use bert embedding feature, bert_doc_emb_file and bert_word_emb_file should be given.")
+    
+    file_name=RunPipeline().run()
+    print(file_name)
+
+if __name__ == '__main__':
+    # Required flag.
+    flags.mark_flag_as_required("data_file")
+    
+    app.run(main)
